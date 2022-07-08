d799389ac59dd82ce380f745473047eeeb8aaa63
yanmifeakeju <yanmifeakeju@gmail.com>
2022-07-09 00:17:21 +0100

    Setup Cucumber E2E tests

---
+++ specs/steps/index.ts
@@ -0,0 +1,32 @@
+import { Then, When } from '@cucumber/cucumber';
+
+When('the client creates a POST request to \\/users', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('attaches a generic empty payload', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('sends the request', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('our API should respond with a {int} HTTP status code', function (int) {
+  // Then('our API should respond with a {float} HTTP status code', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('the payload of the response should be a JSON object', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('contains a message property which says {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
