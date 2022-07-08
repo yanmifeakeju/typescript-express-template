5a3488d384f6025ffb7f71799b2cf4912d44759e
yanmifeakeju <yanmifeakeju@gmail.com>
2022-07-08 22:02:43 +0100

    testing husky

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+/**
+ * Load extra configuration I don't want to share in .env file
+ */
+import convict from 'convict';
+
+const env = process.env.NODE_ENV || 'development';
+
+const config = convict({});
+
+config.loadFile(`config/${env}.json`);
+config.validate({ allowed: 'strict' });
+
+export default config;
