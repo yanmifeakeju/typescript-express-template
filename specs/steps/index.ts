e82a9bd0536536892df74e9188992000939a7223
yanmifeakeju <yanmifeakeju@gmail.com>
2022-08-06 01:53:51 +0100

    Cucumber test case parse and passing correctly

--- specs/steps/index.ts
+++ specs/steps/index.ts
@@ -1,12 +1,9 @@
 import { Then, When } from '@cucumber/cucumber';
+import assert from 'assert';
 import superagent from 'superagent';
 
-let request: superagent.SuperAgentRequest;
-let result: superagent.Response | undefined;
-let error: superagent.Response | undefined;
-
 When('the client creates a POST request to \\/users', function () {
-  request = superagent('POST', 'localhost:5000/users');
+  this.request = superagent('POST', 'localhost:5000/users');
 });
 
 When('attaches a generic empty payload', function () {
@@ -16,25 +13,27 @@ When('attaches a generic empty payload', function () {
 
 When('sends the request', async function () {
   try {
-    result = await request;
+    this.response = await this.request;
   } catch (err) {
     const responseError = err as superagent.ResponseError;
-    error = responseError.response;
+    this.response = responseError.response;
   }
 });
 
-Then('our API should respond with a {int} HTTP status code', function (int: number) {
-  if (error?.statusCode !== 400) {
-    throw new Error(JSON.stringify(result?.body));
-  }
+Then('our API should respond with a {int} HTTP status code', function (statusCode: number) {
+  assert.equal(this.response?.statusCode, 400);
 });
 
 Then('the payload of the response should be a JSON object', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  try {
+    this.responsePayload = JSON.parse(this.response.text);
+  } catch (err) {
+    throw new Error('Malformed JSON response');
+  }
 });
 
-Then('contains a message property which says {string}', function (string) {
+Then('contains a message property which says {string}', function (message: string) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+
+  assert.equal(this.responsePayload.message, message);
 });
