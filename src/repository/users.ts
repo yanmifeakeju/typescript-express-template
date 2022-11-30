77257169049d7902bce37779c880a7e84fa8d952
yanmifeakeju <yanmifeakeju@gmail.com>
2022-11-30 15:42:10 +0100

    change script

--- src/repository/users.ts
+++
@@ -1,8 +0,0 @@
-import { Prisma } from '@prisma/client';
-import { postgresClient } from '../infrastructure/postgres/connection';
-const db = postgresClient.user;
-
-export const createRecord = (data: Prisma.UserCreateInput) => db.create({ data });
-
-export const fetchRecord = (whereInput: Prisma.UserWhereInput, selectInput?: Prisma.UserSelect) =>
-  db.findFirst({ where: { ...whereInput }, ...(selectInput && { select: selectInput }) });
---
+++ src/services/auth/components/register.ts
@@ -0,0 +1,3 @@
+export const registerUser = () => {
+  return 'unimplemented';
+};
