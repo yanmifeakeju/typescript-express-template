b9bb9b73374b92ce9bbc5cd706d347eaa84a2185
yanmifeakeju <yanmifeakeju@gmail.com>
2023-04-17 16:18:00 +0100

    redundant code

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -2,7 +2,8 @@ const appErrorType = {
   INVALID_ARGUMENT: 'INVALID_ARGUMENT',
   ILLEGAL_ARGUMENT: 'ILLEGAL_ARGUMENT',
   DUPLICATE_ENTRY: 'DUPLICATE_ENTRY',
-  NOT_FOUND: 'NOT_FOUND'
+  NOT_FOUND: 'NOT_FOUND',
+  FATAL: 'FATAL'
 } as const;
 
 type AppErrorType = typeof appErrorType[keyof typeof appErrorType];
